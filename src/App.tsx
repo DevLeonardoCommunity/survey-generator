5d5ebba3a5a4d9443b5133db9bf86dff81507c02
Leonardo Montini <lion.48m@gmail.com>
2024-02-05 20:12:23 +0100

    chore: setup shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import "./App.css";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click me</Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </>
   );
 }
