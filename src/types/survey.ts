7199925f3b0783e2e383af9e3542bbffaedefe62
Leonardo Montini <lion.48m@gmail.com>
2024-04-28 18:06:19 +0200

    Add required question functionality

--- src/types/survey.ts
+++ src/types/survey.ts
@@ -12,6 +12,7 @@ export const TextQuestion = v.merge([
     question: v.string([
       v.minLength(3, "Question must be at least 3 characters"),
     ]),
+    required: v.boolean(), // Added required field
   }),
 ]);
 export type TextQuestion = v.Output<typeof TextQuestion>;
@@ -37,6 +38,7 @@ export const ChoiceQuestion = v.merge([
       }),
       [v.minLength(2, "There must be at least 2 options")]
     ),
+    required: v.boolean(), // Added required field
   }),
 ]);
 export type ChoiceQuestion = v.Output<typeof ChoiceQuestion>;
